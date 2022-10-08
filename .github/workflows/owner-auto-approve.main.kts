c459a2c17cd33a08ead406678b7fad10d9bdef1f
Wolf-Martell Montwé <wolf@wolfmontwe.de>
2022-10-08 23:22:01 +0200

    Add Kotlin script to generate the GitHub workflow file

---
+++ .github/workflows/owner-auto-approve.main.kts
@@ -0,0 +1,47 @@
+#!/usr/bin/env kotlin
+
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.28.0")
+
+import it.krzeminski.githubactions.actions.actions.GithubScriptV6
+import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
+import it.krzeminski.githubactions.domain.triggers.WorkflowCall
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.toYaml
+import java.io.File
+
+val flow = workflow(
+    name = "Owner auto approve",
+    on = listOf(WorkflowCall()),
+    sourceFile = __FILE__.toPath(),
+) {
+    job(
+        id = "owner-auto-approve",
+        runsOn = UbuntuLatest,
+        _customArguments = mapOf(
+          "permissions" to mapOf(
+              "pull-requests" to "write"
+          )
+        ),
+    ) {
+        uses(
+            name = "Approve owners review",
+            condition = "github.actor == 'wmontwe' &&  contains(github.event.comment.body, ':+1:')",
+            action = GithubScriptV6(
+                script = """
+                    github.rest.pulls.createReview({
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        pull_number: context.issue.number,
+                        review_id: 1,
+                        event: 'APPROVE',
+                        body: 'This is ready to go.'
+                    })
+                """.trimIndent()
+            )
+        )
+    }
+}
+
+println(flow.toYaml())
+
+File("owner-auto-approve.yaml").writeText(flow.toYaml())
